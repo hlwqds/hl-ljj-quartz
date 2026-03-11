c10256df89f2b0607dd529bd021cba8e75c05242
hlwqds <hl-ljj@outlook.com>
2026-03-11 13:40:26 +0800

    Complete Open Spec with advanced Obsidian features and      config optimization

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -32,8 +32,7 @@ export const sharedPageComponents: SharedLayout = {
   ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/hlwqds/quartz",
     },
   }),
 }
